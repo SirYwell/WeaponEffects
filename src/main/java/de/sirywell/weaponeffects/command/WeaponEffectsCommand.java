ab533a86ce97d6ade4b725a5a04651260716a576
SirYwell <hannesgreule@outlook.de>
2019-05-11 00:24:46 +0200

    Use messages in weapon effects command

--- src/main/java/de/sirywell/weaponeffects/command/WeaponEffectsCommand.java
+++ src/main/java/de/sirywell/weaponeffects/command/WeaponEffectsCommand.java
@@ -4,33 +4,52 @@ import co.aikar.commands.BaseCommand;
 import co.aikar.commands.annotation.CommandAlias;
 import co.aikar.commands.annotation.Default;
 import co.aikar.commands.annotation.Subcommand;
+import de.sirywell.weaponeffects.Messages;
+import de.sirywell.weaponeffects.effect.WeaponEffect;
 import de.sirywell.weaponeffects.effect.WeaponEffectType;
 import de.sirywell.weaponeffects.handler.EffectHandler;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
-@CommandAlias("weaponeffects|weffect")
-public class WeaponEffectsCommand extends BaseCommand {
-    private EffectHandler effectHandler;
+import java.util.Set;
 
-    public WeaponEffectsCommand(EffectHandler effectHandler) {
+@CommandAlias("weaponeffects|weffects|weffect")
+public class WeaponEffectsCommand extends BaseCommand {
+    private EffectHandler<?> effectHandler;
+    private Messages messages;
+
+    public WeaponEffectsCommand(EffectHandler effectHandler, Messages messages) {
         this.effectHandler = effectHandler;
+        this.messages = messages;
     }
 
     @Default
     public void help(Player player) {
-        player.sendMessage("HELP");
+        player.sendMessage(messages.getFormattedMessage("help"));
     }
 
     @Subcommand("show")
     public void showEffects(Player player) {
-        // TODO list all effects of current item
+        if (!hasValidItem(player)) {
+            return;
+        }
+        Set<? extends WeaponEffect> effects = effectHandler.getEffects(player.getInventory().getItemInMainHand());
+        if (effects.isEmpty()) {
+            player.sendMessage(messages.getFormattedMessage("show.no-effects-error"));
+            return;
+        }
+        player.sendMessage(messages.getFormattedMessage("show.header"));
+        effects.forEach(effect -> player.sendMessage(messages.getFormattedMessage("show.line", effect.getType(),
+                effect.getDuration(), effect.getAmplifier(), effect.hasParticles(), effect.hasAmbient())));
     }
 
     @Subcommand("add")
     public void addEffect(Player player, WeaponEffectType type, int duration, @Default("1") short amplifier,
                           @Default("true") boolean ambient, @Default("false") boolean particles,
                           @Default("false") boolean icon) {
+        if (!hasValidItem(player)) {
+            return;
+        }
         if (amplifier > Byte.MAX_VALUE) {
             // TODO warn/error
         }
@@ -38,10 +57,20 @@ public class WeaponEffectsCommand extends BaseCommand {
         ItemStack result = effectHandler.addEffect(type, (byte) amplifier, duration, ambient, particles, icon,
                 player.getInventory().getItemInMainHand());
         boolean success = player.getInventory().getItemInMainHand() != result;
-        if (success)
+        if (success) {
             player.getInventory().setItemInMainHand(result);
-        // TODO send message
-        player.sendMessage("Effect added: " + success);
+            player.sendMessage(messages.getFormattedMessage("add.success",
+                    type, duration, amplifier, particles, ambient));
+        }
+        // TODO send message on fail
+    }
+
+    private boolean hasValidItem(Player player) {
+        if (player.getInventory().getItemInMainHand() == null) {
+            player.sendMessage(messages.getFormattedMessage("no-item-in-hand"));
+            return false;
+        }
+        return true;
     }
 
 }
