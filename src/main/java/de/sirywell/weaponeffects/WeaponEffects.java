70c88ddb7080f9b1651893aaf8c8903ee111c106
Hannes Greule <hannesgreule@outlook.de>
2019-05-12 21:46:29 +0200

    Add autocomplete and byte parsing for command contexts

--- src/main/java/de/sirywell/weaponeffects/WeaponEffects.java
+++ src/main/java/de/sirywell/weaponeffects/WeaponEffects.java
@@ -1,8 +1,13 @@
 package de.sirywell.weaponeffects;
 
+import co.aikar.commands.ACFUtil;
+import co.aikar.commands.BukkitCommandExecutionContext;
+import co.aikar.commands.InvalidCommandArgument;
+import co.aikar.commands.MessageKeys;
 import co.aikar.commands.PaperCommandManager;
 import de.sirywell.weaponeffects.command.WeaponEffectsCommand;
 import de.sirywell.weaponeffects.effect.EfficientEffect;
+import de.sirywell.weaponeffects.effect.WeaponEffectType;
 import de.sirywell.weaponeffects.handler.EffectHandler;
 import de.sirywell.weaponeffects.handler.EfficientEffectHandler;
 import org.bukkit.Bukkit;
@@ -72,6 +77,22 @@ public class WeaponEffects extends JavaPlugin {
 
     private void setupCommands() {
         PaperCommandManager manager = new PaperCommandManager(this);
+
+        manager.enableUnstableAPI("help");
+
+        manager.getCommandContexts().registerContext(byte.class, (c) -> {
+            try {
+                return parseAndValidateNumber(c, Byte.MIN_VALUE, Byte.MAX_VALUE).byteValue();
+            } catch (NumberFormatException e) {
+                throw new InvalidCommandArgument(MessageKeys.MUST_BE_A_NUMBER, "{num}", c.getFirstArg());
+            }
+        });
+
+        manager.getCommandCompletions().registerAsyncCompletion("weaponEffectType",
+                c -> Arrays.stream(WeaponEffectType.values())
+                        .map(WeaponEffectType::name)
+                        .collect(Collectors.toList()));
+
         manager.registerCommand(new WeaponEffectsCommand(effectHandler, messages));
     }
 
@@ -113,5 +134,28 @@ public class WeaponEffects extends JavaPlugin {
         stopTask();
         startTask();
         Bukkit.getLogger().info("Restarted weapon effect task successfully.");
+    }
+
+
+    // see https://github.com/aikar/commands/blob/master/core/src/main/java/co/aikar/commands/CommandContexts.java
+
+
+    private Number parseAndValidateNumber(BukkitCommandExecutionContext c, Number minValue, Number maxValue)
+            throws InvalidCommandArgument {
+        final Number val = ACFUtil.parseNumber(c.popFirstArg(), c.hasFlag("suffixes"));
+        validateMinMax(c, val, minValue, maxValue);
+        return val;
+    }
+
+    private void validateMinMax(BukkitCommandExecutionContext c, Number val, Number minValue, Number maxValue)
+            throws InvalidCommandArgument {
+        minValue = c.getFlagValue("min", minValue);
+        maxValue = c.getFlagValue("max", maxValue);
+        if (maxValue != null && val.doubleValue() > maxValue.doubleValue()) {
+            throw new InvalidCommandArgument(MessageKeys.PLEASE_SPECIFY_AT_MOST, "{max}", String.valueOf(maxValue));
+        }
+        if (minValue != null && val.doubleValue() < minValue.doubleValue()) {
+            throw new InvalidCommandArgument(MessageKeys.PLEASE_SPECIFY_AT_LEAST, "{min}", String.valueOf(minValue));
+        }
     }
 }
